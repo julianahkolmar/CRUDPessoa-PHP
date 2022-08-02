b911baa419d9d1715ec28b30bf23ca20340fb21c
Juliana <87387516+julianahkolmar@users.noreply.github.com>
2022-08-02 17:25:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>CRUD Usuário</title>
+        
+        <!-- CSS Bootstrap -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container text-center">
+        <h1>Tela Inicial</h1>
+        <br/><br/>
+        <button class="btn btn-primary" onclick="location.href='view/cadUsuario.php'">
+            Cadastro de Usuario
+        </button> 
+        <script src="assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</div>
+    </body>
+</html>
